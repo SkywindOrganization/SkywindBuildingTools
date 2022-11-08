81a2d62a61b02aa4e554db9018afe7e52a15d5f3
Unpxre <unpxre.tw@gmail.com>
2022-11-08 16:22:40 +0800

    使用 SwiftFormat 預編譯執行檔 (#7)

--- Package.swift
+++ Package.swift
@@ -12,14 +12,22 @@ let package = Package(
     platforms: [.macOS(.v12)],
     products: [
         .plugin(name: "Formatting", targets: ["Formatting"])
+//        .plugin(name: "Linting", targets: ["Linting"])
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/nicklockwood/SwiftFormat.git",
-            .upToNextMajor(from: Version(0, 0, 0))
-        )
-    ],
+    dependencies: [],
     targets: [
+        .binaryTarget(
+            name: "swiftformat",
+            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.50.3/swiftformat.artifactbundle.zip",
+            checksum: "a3221d54c2ac00f5c0ce0a2ebc6906ee371d527814174a9c65983f3a3a395321"
+        ),
+        // 不知道為什麼 `swiftlint` 總是失敗
+//        .binaryTarget(name: "swiftlint", path: "Per-Build/SwiftLintBinary-macos.artifactbundle.zip")
+//        .binaryTarget(
+//            name: "swiftlint",
+//            url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
+//            checksum: "227258fdb2f920f8ce90d4f08d019e1b0db5a4ad2090afa012fd7c2c91716df3"
+//        ),
         .plugin(
             name: "Formatting",
             capability: .command(
@@ -29,9 +37,16 @@ let package = Package(
                 ]
             ),
             dependencies: [
-                .product(name: "swiftformat", package: "SwiftFormat")
+                .target(name: "swiftformat")
             ],
             path: "Sources/Formatting"
         )
+//        .plugin(
+//            name: "Linting",
+//            capability: .buildTool(),
+//            dependencies: [
+//                .target(name: "swiftlint")
+//            ]
+//        )
     ]
 )
