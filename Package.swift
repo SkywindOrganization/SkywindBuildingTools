9b06aee20bf17b049f36b5c6676ac4777f131ba2
Unpxre <unpxre.tw@gmail.com>
2022-11-08 14:37:40 +0800

    [test]

--- Package.swift
+++ Package.swift
@@ -7,48 +7,71 @@
 
 import PackageDescription
 
-#if true
-// 只有在除錯模式引用原程式碼並編譯
-let dependencies: [Package.Dependency] = [
-    .package(
-        url: "https://github.com/nicklockwood/SwiftFormat.git",
-        .upToNextMajor(from: Version(0, 0, 0))
-    )
-]
-var targets: [Target] = [
-    .executableTarget(name: "formater", dependencies: ["SwiftFormat"], path: "Sources/FormatingTool")
-]
-let formattingDependencies: [Target.Dependency] = [
-    .target(name: "formater")
-]
-#else
-// 釋出模式使用預編譯的可執行檔
-let dependencies: [Package.Dependency] = []
-var targets: [Target] = []
-let formattingDependencies: [Target.Dependency] = []
-#endif
+import class Foundation.ProcessInfo
 
-targets.append(
-    .plugin(
-        name: "Formatting",
-        capability: .command(
-            intent: .sourceCodeFormatting(),
-            permissions: [
-                .writeToPackageDirectory(reason: "格式化時需要修改文件")
-            ]
+let isPerBuild: Bool = true // ProcessInfo.processInfo.environment["PER_BUILDING"] != nil
+
+/// 當環境變數有預建構旗標連結依賴庫進行編譯
+var dependencies: [Package.Dependency] = []
+    + (isPerBuild
+        ? [
+            .package(
+                url: "https://github.com/nicklockwood/SwiftFormat.git",
+                .upToNextMajor(from: Version(0, 0, 0))
+            )
+        ]
+        : [])
+
+var formattingDependencies: [Target.Dependency] = []
+    + (isPerBuild
+        ? [
+            .target(name: "formater")
+        ]
+        : [
+            .target(name: "formater")
+        ])
+
+var targets: [Target] = [
+] + (isPerBuild
+    ? [
+        .executableTarget(
+            name: "formater",
+            dependencies: ["SwiftFormat"],
+            path: "Sources/FormatingTool"
+        )
+    ]
+    : [
+        .target(
+            name: "formater",
+            path: "Per-Build",
+            resources: [.copy("formater")]
         ),
-        dependencies: formattingDependencies,
-        path: "Sources/Formating"
-    )
-)
+        .plugin(
+            name: "Formatting",
+            capability: .command(
+                intent: .sourceCodeFormatting(),
+                permissions: [
+                    .writeToPackageDirectory(reason: "格式化時需要修改文件")
+                ]
+            ),
+            dependencies: formattingDependencies,
+            path: "Sources/Formating"
+        )
+    ])
+
+let products: [Product] = [
+] + (isPerBuild
+    ? [
+        .executable(name: "formater", targets: ["formater"])
+    ]
+    : [
+        .plugin(name: "Formatting", targets: ["Formatting"])
+    ])
 
 let package = Package(
     name: "SkywindBuildingTools",
     platforms: [.macOS(.v12)],
-    products: [
-        .executable(name: "formater", targets: ["formater"]),
-        .plugin(name: "Formatting", targets: ["Formatting"])
-    ],
+    products: products,
     dependencies: dependencies,
     targets: targets
 )
