503a28edbdab484200c45bf0f25fb92573c8c564
Unpxre <unpxre.tw@gmail.com>
2022-11-10 18:01:13 +0800

    預編譯流程測試

--- Tools/setup_formater.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NOW_PATH=$(pwd)
-
-source $(dirname "$0")/setup_tool.sh
-
-setupTool "formater" "0.50.3"
--- Tools/setup_per_build.sh
+++ Tools/setup_per_build.sh
@@ -1,4 +1,12 @@
-echo 預先處理 Per-Build 資料夾
+#!/bin/bash
+# 將需要預先編譯的可執行檔與版本在本腳本進行定義
 
-rm -rf Per-Build
+# 建構工具二進制目錄的方法定義
+source $(dirname "$0")/setup_tool.sh
+
+echo 預先處理 Per-Build 資料夾
 mkdir Per-Build
+
+# 複製工具到特定結構內
+setupTool "formater" "0.50.3"
+setupTool "linter" "0.50.0-rc.4"
